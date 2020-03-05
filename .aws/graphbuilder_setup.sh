69fa29ab1e0bc188569491fd2cfcd9f2072ecd2a
Florian Porada <florian.porada@gmail.com>
2020-03-05 22:19:26 +0100

    feat: update graphbuilder setup script

--- .aws/graphbuilder_setup.sh
+++ .aws/graphbuilder_setup.sh
@@ -1,8 +1,16 @@
 # Setup for instances to build a graph via docker + graphhopper
 
-echo "Install Docker + Git"
+echo "SETUP SCRIPT FOR THE GRAPHBUILDER boii"
+echo "Don't run with 'sudo'"
+sleep 7
+
+echo "Update packages"
 sudo yum update -y
-# sudo yum install git docker -y
+
+echo "Install Git"
+sudo yum install git -y
+
+echo "Install Docker"
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
@@ -23,14 +31,24 @@ bin/osmosis
 
 echo "Osmosis available via ~/osmosis/bin/osmosis"
 
+echo "Create pbf directory"
+cd ~
+mkdir pbf
+
 echo "Get move-lab/graphhopper repo"
 cd ~
 git clone https://github.com/move-lab/graphhopper.git
-cd graphhopper
 
-echo "Opening config.yml for possible RAM changes"
-sleep 5
-vim ./config.yml
-
-echo "Build graphhopper image"
-sudo docker build . -t movelab/graphhopper
+echo "Usage:"
+echo "---"
+echo "---"
+echo "Edit Dockerfile for possible RAM adjustments ('Xmx' and 'Xms' values. Eg. Xmx32G = 32gb RAM)"
+echo "---"
+echo "Build docker image:"
+echo "cd ~/graphhopper && docker build . -t movelab/graphhopper"
+echo "---"
+echo "Download pbf file into pbf folder:"
+echo "cd ~/pbf && wget https://lab-graphhopper-graphdata-prod.s3-eu-west-1.amazonaws.com/england_denmark_germany_merged.pbf"
+echo "---"
+echo "Build graph with docker image:"
+echo "docker run -d -p 8999:8989 -v /home/ec2-user/pbf:/data movelab/graphhopper:latest -i /data/england_denmark_germany_merged.pbf"
